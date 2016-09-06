56d19b0799a672f5720a3f075d529d3f56d9a8d5
Dylan Torres <diland.t95@hotmail.com>
2016-09-06 18:27:40 -0500

    Update syscallsdylan.c

--- 12103021/syscallsdylan.c
+++ 12103021/syscallsdylan.c
@@ -1,21 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main() {
-  int entero;
-   int i;
-  entero = fork();
-  if (p == 0) {
-    sleep(3);
-      printf("Soy el proceso hijo numero %d \n", getpid());
-  }
-  else {
-    i = fork();
-    if(i==0){
-      sleep(5);
-	    printf("Soy el proceso padre numero %d \n", getpid());
-	}else{
-	puts("Hola");
-	}
+main(int argc, char **argv[])
+{
+	
+int archivo;
+char *variable;
+archivo = open("documento.txt",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR); //abre el doc en modo escritura, creat lo crea si no existe aún
+//S_IRUSR|S_IWUSR lectura y escritura para el usuario (Seleccion)
+
+scanf("%s",&variable); //agrego al archivo lo que deseo por teclado
+ 
+printf("el tamano del texto que agregue es %d\n",sizeof(variable)); //muestro el tamaño del texto que se digito
+ 
+write(archivo,&variable,sizeof(variable)); //guardamos lo que se digito en el archivo
+ 
+close(archivo);
 }
-  puts("Fin de los procesos \n");
-}
+
